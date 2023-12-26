30957f957f8eb002e5ee50b4f4321b31993aefe1
Brener Rosa <brenersrosa@gmail.com>
2023-12-26 19:54:44 -0300

    feat: theme toggle and account menu

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async'
 
 export default function Dashboard() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="">
       <Helmet title="Dashboard" />
       <span>Dashboard</span>
     </div>
