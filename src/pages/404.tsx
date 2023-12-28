0f3a7bd6dc2a31f5d465d8cea4099deed9facba1
Brener Rosa <brenersrosa@gmail.com>
2023-12-28 11:57:49 -0300

    feat: page 404 (not found)

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Página não encontrada</h1>
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link
+          to="/"
+          className="text-sky-500 transition-colors hover:text-opacity-85 dark:text-sky-400"
+        >
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
